da4a4cb7b2e841b8bed50b7b272c704c9b51b9ba
Илья <@DESKTOP-ALQEF61>
2021-11-13 18:40:00 +0200

    Add project files.

---
+++ ASP_Trello_Identity/Data/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Trello_Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(127)")]
+        public string FullName { get; set; }
+        [PersonalData]
+        [Column(TypeName = "nvarchar(max)")]
+        public string Info { get; set; }
+    }
+}
